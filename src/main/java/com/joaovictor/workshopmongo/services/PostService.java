5b1e3c2e5b1956e7ac2504dba6370deedff95f1b
joao victor <joaovictormdasilva676@gmail.com>
2022-08-20 20:38:59 -0300

    Consulta simples com @Query

--- src/main/java/com/joaovictor/workshopmongo/services/PostService.java
+++ src/main/java/com/joaovictor/workshopmongo/services/PostService.java
@@ -22,7 +22,7 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String title) {
-		return repository.findByTitleContainingIgnoreCase(title);
+		return repository.searchTitle(title);
 	}
 
 }
